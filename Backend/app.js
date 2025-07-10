f79a518fc8a63b578a4c7ccd4ad515b5a57d92ad
Hassan Azmy <hassanazmy13579@gmail.com>
2025-07-10 03:21:47 +0300

    Replacing bodyParser with express middleware since it's now built into it

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,10 @@
 import express from "express";
-import bodyParser from 'body-parser';
 import feedRouter from './router/feed-router.js';
 
 const app = express(); 
 
-// app.use(bodyParser.urlencoded());   //? Useful for requests that hold data in the format of | x-www-form-urlencoded | through <form> post requests
-app.use(bodyParser.json());            //? Useful for Content-Type of application/json 
+// app.use(express.urlencoded());   //? Useful for requests that hold data in the format of | x-www-form-urlencoded | through <form> post requests
+app.use(express.json());            //? Useful for Content-Type of application/json 
 
 app.use((req, res, next) => {
    //? Allows any domain to access your API
