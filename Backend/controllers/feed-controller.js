b514af314ae87054c83a9384adea5b4821fcc68a
Hassan Azmy <hassanazmy13579@gmail.com>
2025-07-10 03:16:52 +0300

    POST method for posts added

--- Backend/controllers/feed-controller.js
+++ Backend/controllers/feed-controller.js
@@ -11,3 +11,20 @@ export async function getPosts(req, res, next) {
       ]
    });
 }
+
+/** @param {express.Request} req */
+export async function createPost(req, res, next) {
+   const { title } = req.body;
+   const { content } = req.body;
+   console.log(title, content);
+
+   // create post in db
+   res.status(201).json({
+      message: 'Post created successfully!',
+      post: {
+         id: new Date().toISOString(),
+         title,
+         content
+      },
+   });
+}
