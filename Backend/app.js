08ac606b5f90d20b886b42860572e51014d25264
Hassan Azmy <hassanazmy13579@gmail.com>
2025-07-10 03:14:20 +0300

    Creating a routes for feed

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import bodyParser from 'body-parser';
+import feedRouter from './router/feed-router.js';
 
 const app = express(); 
 
 // app.use(bodyParser.urlencoded());   //? Useful for requests that hold data in the format of | x-www-form-urlencoded | through <form> post requests
 app.use(bodyParser.json());            //? Useful for Content-Type of application/json 
+
+app.use('/feed', feedRouter);
 
 app.listen(8080);
