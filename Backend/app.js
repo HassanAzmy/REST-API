0d27f5c7c11d691cf461d07e2d06f70ca1bbeaec
Hassan Azmy <hassanazmy13579@gmail.com>
2025-07-10 03:18:54 +0300

    Specifying the headers needed for CORS errors

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,18 @@ const app = express();
 // app.use(bodyParser.urlencoded());   //? Useful for requests that hold data in the format of | x-www-form-urlencoded | through <form> post requests
 app.use(bodyParser.json());            //? Useful for Content-Type of application/json 
 
+app.use((req, res, next) => {
+   //? Allows any domain to access your API
+   res.setHeader('Access-Control-Allow-Origin', '*');
+
+   //? Specifies which HTTP methods are allowed when accessing the resource
+   res.setHeader('Access-Control-Allow-Methods', 'POST, PUT, PATCH, DELETE');
+
+   //? Specifies which headers can be used in the actual request
+   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+   next();
+});
+
 app.use('/feed', feedRouter);
 
 app.listen(8080);
