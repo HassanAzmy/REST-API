94bdd9c28ac76288cae05aa4f53f5b49916f2e67
Hassan Azmy <hassanazmy13579@gmail.com>
2025-07-10 03:12:52 +0300

    Adding body parser for JSON

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,9 @@
 import express from "express";
+import bodyParser from 'body-parser';
 
 const app = express(); 
+
+// app.use(bodyParser.urlencoded());   //? Useful for requests that hold data in the format of | x-www-form-urlencoded | through <form> post requests
+app.use(bodyParser.json());            //? Useful for Content-Type of application/json 
 
 app.listen(8080);
